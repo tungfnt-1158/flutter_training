d14a34ecec7ea69cd1a567feae13624264c30d8e
tungfnt-1158 <nguyen.thanh.tungf@sun-asterisk.com>
2020-09-09 10:23:44 +0700

    first commit

---
+++ HomeWork_LayoutView/README.md
@@ -0,0 +1,4 @@
+## What I Did:
+- what new?
+
+## Evidence (Screenshot or Video)
---
+++ HomeWork_LayoutView/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'common_widget.dart';
+
+main() async {
+  return runApp(
+    MaterialApp(
+      home: CommonWidget(),
+    ),
+  );
+}
